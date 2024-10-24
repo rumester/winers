4c82070c1924bace11c84e9f657b81fee9febcea
missrce <vcs@rce.run>
2024-10-24 18:49:07 +0100

    feat: test script

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use winers::Wine;
+
 fn main() {
-	println!("winers debug - meow :3");
+    let prefix_path = "/home/missrce/.local/share/rumester/wine"; // prefix these
+    let prefix = Wine::new(&prefix_path);
+
+    match prefix.init() {
+        Ok(_) => println!("Wine prefix initialized successfully at '{}'.", prefix_path),
+        Err(e) => println!("Error: {}", e)
+    };
+
+    let output = prefix.cmd()
+        .arg("notepad")
+        .output()
+        .expect("how the hell did notepad fail to run");
+
+    println!("status: {}", output.status);
+
+    match prefix.delete() {
+        Ok(_) => println!("Wine prefix deleted successfully at '{}'.", prefix_path),
+        Err(e) => print!("Error: {}", e.to_string())
+    };
 }
