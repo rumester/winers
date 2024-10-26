f1d166e4b02dbe35bdf0bf3c377884322048b824
V3L0C1T13S <turtletester20@gmail.com>
2024-10-26 00:22:59 -0400

    tests: add simple dxvk install test

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use winers::{get_latest_dxvk, Wine};
 use std::env;
+use winers::{get_latest_dxvk, install_dxvk, Wine};
 
 #[tokio::main]
 async fn main() {
     println!("winers debug - meow :3");
-    
+
     let home = env::var("HOME").expect("could not get the HOME variable");
 
     let prefix_path = format!("{}/.wine", home);
@@ -12,10 +12,11 @@ async fn main() {
 
     match prefix.init() {
         Ok(_) => println!("Wine prefix initialized successfully at '{}'.", prefix_path),
-        Err(e) => println!("Error: {}", e)
+        Err(e) => println!("Error: {}", e),
     };
 
-    let output = prefix.cmd()
+    let output = prefix
+        .cmd()
         .arg("notepad")
         .output()
         .expect("how the hell did notepad fail to run");
@@ -24,28 +25,36 @@ async fn main() {
 
     match prefix.reg_add(r"HKEY_CURRENT_USER\WINERS", "DidItWork", "REG_DWORD", "1") {
         Ok(_) => println!("Successfully created the registry key!"),
-        Err(e) => println!("Error: {}", e)
+        Err(e) => println!("Error: {}", e),
     };
 
-    prefix.cmd()
+    prefix
+        .cmd()
         .arg("regedit")
         .output()
         .expect("regedit startup epic failure");
 
     match prefix.reg_delete(r"HKEY_CURRENT_USER\WINERS", "DidItWork") {
         Ok(_) => println!("Successfully deleted the registry key!"),
-        Err(e) => println!("Error: {}", e)
+        Err(e) => println!("Error: {}", e),
     };
 
-    prefix.cmd()
+    prefix
+        .cmd()
         .arg("regedit")
         .output()
         .expect("regedit startup epic failure");
 
-    get_latest_dxvk().await;
+    let latest_dxvk = get_latest_dxvk()
+        .await
+        .expect("epic dxvk version fetch fail");
+    match install_dxvk(&prefix, latest_dxvk.as_str()).await {
+        Ok(_) => println!("Successfully installed dxvk."),
+        Err(e) => eprintln!("Error installing dxvk: {e}"),
+    }
 
     match prefix.delete() {
         Ok(_) => println!("Wine prefix deleted successfully at '{}'.", prefix_path),
-        Err(e) => print!("Error: {}", e.to_string())
+        Err(e) => print!("Error: {}", e.to_string()),
     };
 }
