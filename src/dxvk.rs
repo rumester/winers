27a01309d90addfcafaf625922ef410741bd35bb
missrce <vcs@rce.run>
2024-10-27 03:56:56 +0000

    feat: add registry overrides to ensure dxvk is used out of the box

--- src/dxvk.rs
+++ src/dxvk.rs
@@ -11,6 +11,14 @@ const REPO: &str = "dxvk";
 const SYS32: &str = "drive_c/windows/system32";
 const SYSWOW64: &str = "drive_c/windows/syswow64";
 
+const DLL_LIST: [&str; 5] = [
+    "d3d8",
+    "d3d9",
+    "d3d10core",
+    "d3d11",
+    "dxgi"
+];
+
 pub async fn get_latest_dxvk() -> Result<String, reqwest::Error> {
     utils::get_latest_release(&OWNER, &REPO).await
 }
@@ -55,6 +63,12 @@ pub async fn install_dxvk(prefix: &Wine, version: &str) -> Result<(), Box<dyn st
         }
     }
 
+    for dll in DLL_LIST {
+        if let Err(e) = prefix.reg_add(r"HKEY_CURRENT_USER\Software\Wine\DllOverrides", &dll, "REG_SZ", "native") {
+            return Err(format!("Failed to add registry key: {}", e).into());
+        }
+    }
+
     Ok(())
 }
 
@@ -62,17 +76,10 @@ pub fn remove_dxvk(prefix: &Wine) -> Result<(), String> {
     let x64_dir = &prefix.prefix_path.join(SYS32);
     let x32_dir = &prefix.prefix_path.join(SYSWOW64);
 
-    let dlls = [
-        "d3d8.dll",
-        "d3d9.dll",
-        "d3d10core.dll",
-        "d3d11.dll",
-        "dxgi.dll"
-    ];
-
-    for dll in dlls {
-        let x64_dir_path = x64_dir.join(dll);
-        let x32_dir_path = x32_dir.join(dll);
+    for dll in DLL_LIST {
+        let dll_path = format!("{}.dll", dll);
+        let x64_dir_path = x64_dir.join(&dll_path);
+        let x32_dir_path = x32_dir.join(&dll_path);
         if x64_dir_path.exists() {
             if let Err(e) = fs::remove_file(&x64_dir_path) {
                 return Err(format!("Failed to delete {}: {}", &x64_dir_path.display(), e.to_string()));
@@ -81,6 +88,11 @@ pub fn remove_dxvk(prefix: &Wine) -> Result<(), String> {
         if x32_dir_path.exists() {
             if let Err(e) = fs::remove_file(&x32_dir_path) {
                 return Err(format!("Failed to delete {}: {}", &x32_dir_path.display(), e.to_string()));
+            }
+        }
+        for dll in DLL_LIST {
+            if let Err(e) = prefix.reg_delete(r"HKEY_CURRENT_USER\Software\Wine\DllOverrides", &dll) {
+                return Err(format!("Failed to delete registry key: {}", e).into());
             }
         }
     }
